80652c4a15583ab46348f2fe0d5ebe66e2b520ae
revanth892 <55280048+revanth892@users.noreply.github.com>
2023-05-24 17:22:44 +0530

    creatind the slice

---
+++ src/features/expenseSlicer.js
@@ -0,0 +1,11 @@
+import { createSlice } from "@reduxjs/toolkit";
+const expense=createSlice(
+    {
+        name:"expense",
+        initialState:[],
+        reducers:
+        {
+            addTransaction(state,action){}
+        }
+    }
+);
